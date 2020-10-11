3acbe62194d4ca9fdaaded3ad5690efb882870e6
pairofAces <karanchauhan16@gmail.com>
2020-10-11 16:10:18 -0400

    backend data

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.1'
 
+gem 'pry'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.3'
 # Use postgresql as the database for Active Record
@@ -23,7 +25,7 @@ gem 'puma', '~> 4.1'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-# gem 'rack-cors'
+gem 'rack-cors'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -39,3 +41,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'dotenv'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'pry'
+require 'rest-client'
+
+url = ENV["api_url"]
+
+count = 1
+while count < 270 do
+
+    case count
+    when 1
+        puts " "
+        puts "Beginning to seed data...please be patient...🌱"
+        puts " "
+    when 50
+        puts "Just getting started...🍀"
+        puts " "
+    when 100
+        puts "25% complete 🌿"
+        puts " "
+    when 150
+        puts "50% complete 🌵"
+        puts " "
+    when 200
+        puts "Yes, still seeding...but growing 🌴"
+        puts " "
+    when 250
+        puts "95% complete 🌳"
+        puts " "
+    end
+
+    begin
+        country_data = JSON.parse(RestClient.get(url))
+        binding.pry
+        
+    rescue RestClient::ExceptionWithResponse => e
+        e.response
+        count += 1
+    end
+
